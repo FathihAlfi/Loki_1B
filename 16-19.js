bd426de20fea180d81d5ba124839352d0b6ba210
anandaftrr <anandafitria.unand@gmail.com>
2022-03-27 21:16:36 +0700

    nanda 16-19

--- 16-19.js
+++ 16-19.js
@@ -1,27 +1,27 @@
 const express = require("express")
 const server = express()
-const port = 5781
+const port = 8000
 
 server.listen (port, function(){
     console.log("Halo! Selamat Datang di Halaman Home Kami")
 })
 
 //fungsional16
-server.get("/edit", function(req,res){
+server.put("/edit", function(req,res){
     res.send("Mengubah Komponen Nilai")
 })
 
 //fungsional17
-server.get("/delete", function(req,res){
+server.delete("/delete", function(req,res){
     res.send("Menghapus Komponen Penilaian")
 })
 
 //fungsional18
-server.get("/add", function(req,res){
+server.put("/add", function(req,res){
     res.send("Menambah Pertemuan Mingguan RPS")
 })
 
 //fungsional19
-server.get("/change", function(req,res){
+server.put("/change", function(req,res){
     res.send("Mengubah Pertemuan Mingguan RPS")
 })
