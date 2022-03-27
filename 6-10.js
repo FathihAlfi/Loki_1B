5ba4c9fe905aec5ec74f6d7c838dd51ea86b8aed
FathihAlfi <91770998+FathihAlfi@users.noreply.github.com>
2022-03-27 21:12:19 +0700

    revisi 6-10.js

--- 6-10.js
+++ 6-10.js
@@ -1,37 +1,37 @@
 const express = require("express")
-const webserver = express()
+const server = express()
 const port = 6900
 
-webserver.listen (port, function(){
+server.listen (port, function(){
     console.log("Assalamualaikum brother")
 })
 
 //Dosen dapat menambahkan RPS baru
-webserver.post('/dosen-nambah-rps', (req, res) =>
+server.post('/dosen-nambah-rps', (req, res) =>
 {
     res.send("rps berhasil ditambahkan")
 })
 
 //Dosen dapat mengubah rps
-webserver.put('/dosen-ubah-rps', (req, res) =>
+server.put('/dosen-ubah-rps', (req, res) =>
 {
     res.send("rps berhasil diubah")
 })
 
 //Dosen dapat merevisi rps
-webserver.put('/dosen-revisi-rps', (req, res) =>
+server.put('/dosen-revisi-rps', (req, res) =>
 {
     res.send("revisi gagal, rps masih kosong")
 })
 
 //Dosen dapat menambah CPMK mata kuliah
-webserver.post('/dosen-nambah-cpmk', (req, res) =>
+server.post('/dosen-nambah-cpmk', (req, res) =>
 {
     res.send("cpmk berhasil ditambahkan")
 })
 
 //Dosen dapat mengubah CPMK mata kuliah
-webserver.put('/dosen-ubah-cpmk', (req, res) =>
+server.put('/dosen-ubah-cpmk', (req, res) =>
 {
     res.send("cpmk berhasil diubah")
 })
