275d610174cbbc61ce4a00f0938cf56deaa30a0b
anandaftrr <anandafitria.unand@gmail.com>
2022-04-07 13:31:20 +0700

    udah rapi ygy2

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log ('SELAMAT MENGERJAKAN SEMOGA A NI ANJAY');
+const database = require('./config/database.js')
+const express = require('express')
+const app = express()
+const port = 8000
+const server = require('./routes/index.js')
+
+database
+  .authenticate()
+  .then(() => {
+    console.log('Berhasil terhubung database');
+  })
+  .catch(err => {
+    console.error('Gagal terhubung :', err);
+  });
+
+app.use('/', server.user)
+
+app.listen(port, () =>
+{
+    console.log(`Server sudah berjalan di port ${port}`)
+})
