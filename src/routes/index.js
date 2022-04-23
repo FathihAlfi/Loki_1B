4ebdb36059faf71e50990bdaedc0dd821f1ad62b
Lathifnurirsyad <70887529+Lathifnurirsyad@users.noreply.github.com>
2022-04-23 11:41:45 +0700

    login logout oke

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const user = require('./user.js')
 const migrationstest = require('./migrationstest.js')
+const auth = require('./auth.js')
+const logout = require('./logout.js')
 
 const server = {}
 
 server.user = user
 server.migrationstest = migrationstest
+server.auth = auth
+server.logout = logout
 
 module.exports = server
