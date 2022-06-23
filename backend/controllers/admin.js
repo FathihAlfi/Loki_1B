0abe83a4f54bdcf563f692090fe41f1553be1575
Lathifnurirsyad <70887529+Lathifnurirsyad@users.noreply.github.com>
2022-06-23 23:15:37 +0700

    .

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -57,7 +57,68 @@ controllers.hlmTambahAksesDosen = async (req, res) => {
     const nama = payload.nama
     const NIP = payload.NIP
 
-    res.render("tambahDosen")
+    const dosen = await models.lecturers.findAll({})
+
+    res.render("daftarDosen", {dosen, id, nama, name, NIP})
+}
+
+controllers.cekTambahAksesDosen = async (req, res) => {
+    const id = req.params.id
+    const idDosen = req.params.idDosen
+    const name = req.params.name
+    const accessToken = req.cookies.accessToken 
+    if (!accessToken)
+        return res.status(200).json("tidak ada token")
+    const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    const id_dosen = payload.id
+    const nama = payload.nama
+    const NIP = payload.NIP
+
+    const dosen = await models.lecturers.findOne({
+        where : {
+            id : idDosen
+        }
+    })
+    res.render("tambahDosen", {dosen, idDosen, id, nama, name, NIP})
+}
+
+controllers.tambahAksesDosen = async (req, res) => {
+    try {
+        const id = req.params.id
+    const idDosen = req.params.idDosen
+    const name = req.params.name
+    const accessToken = req.cookies.accessToken 
+    if (!accessToken)
+        return res.status(200).json("tidak ada token")
+    const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    const id_dosen = payload.id
+    const nama = payload.nama
+    const NIP = payload.NIP
+
+    await models.course_plan_lecturers.create({
+        course_plan_id : id,
+        lecturer_id : idDosen,
+        creator : 0
+    })
+    res.status(200).redirect("/detailAksesDosen/"+id+"/"+name)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+controllers.semuaAksesDosen = async (req, res) => {
+    const accessToken = req.cookies.accessToken 
+    if (!accessToken)
+        return res.status(200).json("tidak ada token")
+    const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    const id = payload.id
+    const nama = payload.nama
+    const NIP = payload.NIP
+
+    const RPS = await models.course_plans.findAll({
+        atribute : ['rev', 'code', 'name', 'credit', 'semester']
+    })
+    res.render("aksesDosen", {RPS, accessToken, nama, NIP})
 }
 
 module.exports = controllers
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -10,7 +10,10 @@ server.get('/admin', (req, res) => {
 server.post('/loginAdmin', controllers.auth.loginAdmin)
 server.get('/homeAdmin', cekLogin, controllers.admin.home)
 server.get('/detailAksesDosen/:id/:name', cekLogin, controllers.admin.detailAksesDosen)
-server.get('/tambahAksesDosen/:id/:name', cekLogin, )
-server.post('/tambahAksesDosen/:id/:name', cekLogin, )
+server.get('/tambahAksesDosen/:id/:name', cekLogin, controllers.admin.hlmTambahAksesDosen)
+server.get('/tambahAksesDosen/:idDosen/:id/:name', cekLogin, controllers.admin.cekTambahAksesDosen)
+server.post('/tambahAksesDosen/:idDosen/:id/:name', cekLogin, controllers.admin.tambahAksesDosen)
+
+server.get('/semuaAksesDosen', cekLogin, controllers.admin.semuaAksesDosen)
 
 module.exports = server
