02963b467a6578ef9d5d058903ce9dc51d28d31e
Lathifnurirsyad <70887529+Lathifnurirsyad@users.noreply.github.com>
2022-06-29 18:44:08 +0700

    .

--- backend/controllers/mhs.js
+++ backend/controllers/mhs.js
@@ -16,4 +16,61 @@ controllers.detail = async(req, res) => {
     res.render("landingpage", {RPS} )
 }
 
+controllers.lihatDetailRPS = async (req, res) => {
+    const id = req.params.id
+    const name = req.params.name
+
+    models.course_los.hasMany(models.course_lo_details, {foreignKey : "id"})
+    models.course_lo_details.belongsTo(models.course_los, {foreignKey : "course_lo_id"})
+    
+    const RPS = await models.course_plans.findOne({
+        where : {id : id}
+    })
+    const CPL = await models.course_lo_details.findAll({
+        include : {
+            model: models.course_los,
+            where : {course_plan_id : id}
+        }
+    })
+    const ref = await models.course_plan_references.findAll({
+        where : {course_plan_id : id}
+    })
+    const pertemuan = await models.course_plan_details.findAll({
+        where : {course_plan_id : id}
+    })
+    const komponen = await models.course_plan_assessments.findAll({
+        where : {course_plan_id : id}
+    })
+    res.render("detailFileRPS", {RPS, CPL, ref, pertemuan, komponen})
+}
+
+controllers.exportDetailRPS = async (req, res) => {
+    target="_blank"
+    const id = req.params.id
+    const name = req.params.name
+
+    models.course_los.hasMany(models.course_lo_details, {foreignKey : "id"})
+    models.course_lo_details.belongsTo(models.course_los, {foreignKey : "course_lo_id"})
+    
+    const RPS = await models.course_plans.findOne({
+        where : {id : id}
+    })
+    const CPL = await models.course_lo_details.findAll({
+        include : {
+            model: models.course_los,
+            where : {course_plan_id : id}
+        }
+    })
+    const ref = await models.course_plan_references.findAll({
+        where : {course_plan_id : id}
+    })
+    const pertemuan = await models.course_plan_details.findAll({
+        where : {course_plan_id : id}
+    })
+    const komponen = await models.course_plan_assessments.findAll({
+        where : {course_plan_id : id}
+    })
+    res.render("PrintRPS", {RPS, CPL, ref, pertemuan, komponen})
+}
+
 module.exports = controllers
